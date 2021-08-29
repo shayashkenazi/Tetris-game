317094ce34d1bfcfb0e145cb47ce2347a1971672
shayashkenazi <shayas9529@gmail.com>
2021-08-29 12:49:40 +0300

    n

--- ProjectTetris1/TetrisGame.cpp
+++ ProjectTetris1/TetrisGame.cpp
@@ -446,7 +446,7 @@ void TetrisGame::RunPlayerVsComputer(Objects** S1, Objects** S2)
 			computer_commands = UpdateComputerPlayersBoard(S2, level, Computer_Player2);
 			index = 0;
 		}
-		Sleep(Sleep1);
+		Sleep(CommandLoopSleep);
 	} while (key != ESC);
 
 }
@@ -505,7 +505,7 @@ void TetrisGame::RunComputerVsComputer(Objects** S1, Objects** S2)
 	   index1++;
 	   index2++;
 
-	   if (ThePlayers[Player1]->getBoardGame().CheckIfExistAtRow(Bottom-6) && computer_commands1[index1] != 's' && flag1)
+	   if (ThePlayers[Player1]->getBoardGame().CheckIfExistAtRow(Bottom-6) && computer_commands1[index1] != 's' && flag1)//moves shape faster to avoid getting stuck
 	   {
 		  FastComputerLoop(**S1, computer_commands1, index1,Player1);
 		  flag1 = false;
